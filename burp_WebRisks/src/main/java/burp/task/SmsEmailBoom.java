e3776304a65e7e3384790e48cb403570caf572f8
alumm0x <zhoufeng9186@cvte.com>
2022-08-08 17:13:26 +0800

    修复bug：修复form表单参数的最后一个参数没法区分边界，导致识别不准确的问题

--- burp_WebRisks/src/main/java/burp/task/SmsEmailBoom.java
+++ burp_WebRisks/src/main/java/burp/task/SmsEmailBoom.java
@@ -38,14 +38,14 @@ public class SmsEmailBoom extends VulTaskImpl {
             //如果请求参数中有邮箱，尝试下重放
             if (request_body_str.length() > 0 || query.length() > 0){
                 //先检测是否存在url地址的参数，正则匹配
-                String phoneRegex = "['\"&<;\\s/,=]+?1(3\\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\\d|9[0-35-9])\\d{8}['\"&<;\\s/,]?"; //手机号的正则
+                String phoneRegex = "['\"&<;\\s/,=]+?1(3\\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\\d|9[0-35-9])\\d{8}['\"&<;\\s/,]+?"; //手机号的正则
                 String emailRegex = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*"; //邮箱的正则
                 Pattern patternPhone = Pattern.compile(phoneRegex);
                 Pattern patternEmail = Pattern.compile(emailRegex);
-                Matcher matcherPhone_body = patternPhone.matcher(request_body_str);
-                Matcher matcherPhone_query = patternPhone.matcher(query);
-                Matcher matcherEmail_body = patternEmail.matcher(request_body_str);
-                Matcher matcherEmail_query = patternEmail.matcher(query);
+                Matcher matcherPhone_body = patternPhone.matcher(request_body_str + "&"); // 为啥加&，为了区分边界，防止最后一个参数没法识别结束，主要是应付form表单
+                Matcher matcherPhone_query = patternPhone.matcher(query + "&");
+                Matcher matcherEmail_body = patternEmail.matcher(request_body_str + "&");
+                Matcher matcherEmail_query = patternEmail.matcher(query + "&");
                 if (matcherEmail_body.find() || matcherEmail_query.find()
                         || matcherPhone_body.find() || matcherPhone_query.find()){
                     for (int i = 0 ; i < 10 ; i++) { //重放10次
